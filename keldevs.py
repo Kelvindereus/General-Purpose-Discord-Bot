7b6ae4573bec408c507feb1da3f067f828ef2b14
Kelvindereus <31496522+Kelvindereus@users.noreply.github.com>
2022-05-28 19:59:24 +0200

    Update keldevs.py

--- keldevs.py
+++ keldevs.py
@@ -1,78 +1,86 @@
+from email import message
+from re import M
 import discord
 import asyncio
 from discord.ext import commands
 import time
+import mysql.connector
+
 prefix = "!keldevs."
 bot = commands.Bot(command_prefix=prefix)
-channel = bot.get_channel("979410173665878016")
+
+messaged = []
+@bot.event
+async def on_ready():
+        print("Alles is klaar om te goan!")
+
+
 
 
 @bot.event
-async def on_ready():
-    print("Alles is klaar om te goan!")
+async def on_message(message):
+    global messaged
 
-@bot.command()
-async def helpmij(ctx):
-    embedVar = discord.Embed(title="KelDevs Commands!", description="I'm a pretty little hot Discord bot, written and hosted by Kelvin!", color=0x00ff00)
-    embedVar.add_field(name="Commando scherm", value="!keldevs.help", inline=True)             
-    embedVar.add_field(name="Jemoeder joke", value="!keldevs.jemoeder", inline=True)
-    embedVar.add_field(name="Admin-Delete messages", value="!keldevs.purge", inline=True)        
-    embedVar.add_field(name="Admin-Announce", value="!keldevs.announce", inline=True)          
-    embedVar.add_field(name="Zie bot message latency", value="!keldevs.ping", inline=True)             
-    embedVar.add_field(name="Zie bot database latency", value="!keldevs.dbping", inline=True) 
-    await ctx.send(embed=embedVar)
+    if message.content.lower() == "!keldevs.jm":
+        await message.channel.send("Je moeder!")
 
-@bot.command()
-async def jemoeder(ctx):
-    latency = bot.latency 
-    await ctx.send("Je moeder")
+    elif message.content.lower() == "!keldevs.pingtest":
+        await pingtest(message)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
+    elif message.content.lower() == "!keldevs.helpmij":
+        await helpmij(message)
 
-@bot.command()
-async def dbping(ctx):
-    latency = bot.latency 
-    await ctx.send(latency)
+    elif message.content.lower() == "!keldevs.sysops":
+        await sysops(message)
 
-@bot.command()
-async def announce(ctx, channel: discord.TextChannel, arg1):
-    embed=discord.Embed(title="Aankondiging", description="{}".format(arg1), color=0xFF5733)
-    await channel.send(embed=embed)
+    elif message.content.lower() == "ad.purge":
+            if message.author.guild_permissions.administrator:
+                p = 0
+                p = int(message.content.split(" ")[1])
+                if (p != 0):
+                    purge(limit, m)
+                else:
+                    await message.channel.send("Flapdrol, ik moet wel weten hoeveel berichten ik moet verwijderen")
+            else:
+                await message.channel.send("Sorry, deze functie is niet toegankelijk voor het plebs... uh ik bedoel mensen zonder administrator permissies")
 
-@bot.command()
-@commands.has_role('「🕶」ServerDeveloper (Admin)')
-async def BotSysOps(ctx):
-    embedVar = discord.Embed(title="KelDevs SysOps info!!", description="I'm a pretty little hot Discord bot, written and hosted by Kelvin!", color=0x00ff00)
-    embedVar.add_field(name="IP", value="10.0.0.22", inline=True)             
-    embedVar.add_field(name="VMID", value="108", inline=True)
-    embedVar.add_field(name="BotPAD", value="/home/bot/keldevs.py", inline=True)          
-    await ctx.send(embed=embedVar)
+    elif message.content.lower() == "!keldevs.birtdayin":
+        await birthday(message)
+   
 
-@bot.command(name="purge")
-@commands.has_role('「👑」Oprichter, 「👑」Administrator, Moderator')
-async def clean(ctx, limit: int):
-        await ctx.channel.purge(limit=limit)
-        await ctx.send("Er zijn {} verwijderd".format(int), delete_after=5)
-        
-@clean.error
-async def clear_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("Je mag dat niet doen")
+async def pingtest(message):
+    before = time.monotonic()
+    message = await message.channel.send("Pong!")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"Pong!  `{int(ping)}ms`")
+
+async def helpmij(message):
+        embedVar = discord.Embed(title="KelDevs Commands!", description="I'm a pretty little hot Discord bot, written and hosted by Kelvin!", color=0x00ff00)
+        embedVar.add_field(name="Commando scherm", value="!keldevs.help", inline=True)             
+        embedVar.add_field(name="Jemoeder joke", value="!keldevs.jemoeder", inline=True)
+        embedVar.add_field(name="Admin-Delete messages", value="!keldevs.purge", inline=True)        
+        embedVar.add_field(name="Admin-Announce", value="!keldevs.announce", inline=True)          
+        embedVar.add_field(name="Zie bot message latency", value="!keldevs.ping", inline=True)             
+        embedVar.add_field(name="Zie bot database latency", value="!keldevs.dbping", inline=True) 
+        message = await message.channel.send(embed=embedVar)
 
 
+async def sysops(message):
+        embedVar = discord.Embed(title="KelDevs SysInfo!", description="I'm a pretty little hot Discord bot, written and hosted by Kelvin!", color=0x00ff00)
+        embedVar.add_field(name="IP", value="10.0.0.22", inline=True)             
+        embedVar.add_field(name="VMID", value="108", inline=True)
+        message = await message.channel.send(embed=embedVar)
 
+async def purge(limit, message):
+    global guild
+    await message.channel.purge(limit=l)
+    s = await message.channel.send("purged by " + message.author.name)
+    sleep(5000)
+    await message.delete()
+    await s.delete()
 
-
-
-
-
-
-
-
-
-
+async def birtdayin(message):
+    
 
 
 
@@ -80,6 +88,19 @@ async def clear_error(ctx, error):
 
 
 
-bot.run('')  # Where 'TOKEN' is your bot token
+
+
+
+
+
+
+
+
+
+
+
+bot.run('DETOKEN')  # Dit is de bot token!
+
+
 
   
